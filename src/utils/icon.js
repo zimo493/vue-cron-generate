8f235130dcde591737880147d8cb42f497bef6ad
huzimo <2081182432@qq.com>
2024-06-20 14:50:12 +0800

    ✨ update

--- src/utils/icon.js
+++
@@ -1,5 +0,0 @@
-import { Icon } from "@iconify/vue";
-import { NIcon } from "naive-ui";
-export function renderIcon(icon, props) {
-    return () => h(NIcon, props, { default: () => h(Icon, { icon }) });
-}
